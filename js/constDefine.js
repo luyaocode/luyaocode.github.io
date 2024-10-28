77531b813dc6c51e774a9b5e169b6239cadb60de
luyaocode <316774040@qq.com>
2024-10-28 11:29:01 +0800

    auth

--- js/constDefine.js
+++ js/constDefine.js
@@ -1,3 +1,3 @@
-// const backend_url = "https://api.chaosgomoku.fun:5001";
+const backend_url = "https://api.chaosgomoku.fun:5001";
 // 本地测试时修改为
-const backend_url = "http://localhost:5001";
+// const backend_url = "http://localhost:5001";

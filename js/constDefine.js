de0bc5baf65d2cb545aabeada3aff827b8544c0d
luyaocode <316774040@qq.com>
2024-11-03 22:49:54 +0800

    fix

--- js/constDefine.js
+++ js/constDefine.js
@@ -1,6 +1,6 @@
 // 线上环境
 const backend_url = "https://api.chaosgomoku.fun:5001";
-const backend_yingwu = "https://api.chaosgomoku.fun:8080";
+const backend_yingwu = "http://api.chaosgomoku.fun:8080";
 // 本地测试时修改为
 // const backend_url = "http://localhost:5001";
 // const backend_yingwu = "http://localhost:8080";

da44a51ad90b5a45a76b6ca459e4776eb8014f55
luyaocode <316774040@qq.com>
2024-11-04 14:52:07 +0800

    update url

--- js/constDefine.js
+++ js/constDefine.js
@@ -4,3 +4,13 @@ const backend_yingwu = "https://api.chaosgomoku.fun:8080";
 // 本地测试时修改为
 // const backend_url = "http://localhost:5001";
 // const backend_yingwu = "http://localhost:8080";
+
+// url
+const Homepage="/";
+const Page_Blog_Home = "/blog/home";
+const Page_Blog_Blog = "/md/page/detail";
+const Page_Blog_Setting = "/blog/setting";
+const Page_Blog_Update = "/md/page/update";
+const Page_Blog_Write = "/md/page/write";
+
+const Page_Yingwu_Home = "/YingWuNetDisk/home";
